de71bb90395a0518102d3ef7023c589f420c59f5
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-06-14 03:28:30 +0800

    Learned about how events work and event driven architecture

--- event-loop.js
+++ event-loop.js
@@ -1,38 +1,3 @@
-// const fs = require("fs");
-// const crypto = require("crypto");
-
-// const start = Date.now();
-
-// setTimeout(() => console.log("Timer 1 finished"), 0);
-// setImmediate(() => console.log("Immediate 1 finished"));
-// fs.readFile("test-file.txt", "utf-8", () => {
-//   console.log("I/O finished");
-//   console.log("---------");
-
-//   setTimeout(() => console.log("Timer 2 finished"), 0);
-//   setTimeout(() => console.log("Timer 3 finished"), 3000);
-//   setImmediate(() => console.log("Immediate 2 finished"));
-
-//   process.nextTick(() => console.log("Process.nextTick"));
-
-//   crypto.pbkdf2('password', 'salt', 100000, 1024, "sha512", () => {
-//     console.log(Date.now() , "Password encrypted 12");
-//   })
-//   crypto.pbkdf2('password', 'salt', 100000, 1024, "sha512", () => {
-//     console.log(Date.now() - start, "Password encrypted");
-//   })
-//   crypto.pbkdf2('password', 'salt', 100000, 1024, "sha512", () => {
-//     console.log(Date.now() - start, "Password encrypted");
-//   })
-//   crypto.pbkdf2('password', 'salt', 100000, 1024, "sha512", () => {
-//     console.log(Date.now() - start, "Password encrypted");
-//   })
-//   crypto.pbkdf2('password', 'salt', 100000, 1024, "sha512", () => {
-//     console.log(Date.now() - start, "Password encrypted");
-//   })
-// });
-// console.log("Hello from the top-level code");
-
 const fs = require("fs");
 const crypto = require("crypto");
 const start = Date.now();
--- event.js
+++ event.js
@@ -39,4 +39,42 @@
 //     console.log("Waiting for request")
 // })
 
-// const Event
+const EventEmitter = require("events");
+const http = require("http");
+class Sales extends EventEmitter {
+  constructor() {
+    super();
+  }
+}
+const myEmitter = new Sales();
+
+myEmitter.on("newSale", () => {
+  console.log("There was a new sale!");
+});
+myEmitter.on("newSale", () => {
+  console.log("Customer name: Jonas!");
+});
+myEmitter.on("newSale", (stock) => {
+  console.log(`There are now ${stock} items left in stock`);
+});
+myEmitter.emit("newSale", 9);
+
+////////////////////////////////////////
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  console.log("request received");
+  console.log(req.url);
+  res.end("Request received");
+});
+server.on("request", (req, res) => {
+  console.log("Another received 😊");
+});
+
+server.on("close", () => {
+  console.log("Server closed");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Waiting for request...");
+});
