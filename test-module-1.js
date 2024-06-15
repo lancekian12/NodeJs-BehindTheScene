59913a1f0358064999fb60bdef79fca9c293e429
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-06-15 14:50:01 +0800

    Learned how to require modules worked

--- test-module-1.js
+++ test-module-1.js
@@ -1,23 +1,23 @@
-// class Calculator{
-//     add(a, b){
-//         return a + b;
-//     }
-//     multiply(a, b){
-//         return a * b;
-//     }
-//     divide(a, b){
-//         return a / b;
-//     }
+// class Calculator {
+//   add(a, b) {
+//     return a + b;
+//   }
+//   multiply(a, b) {
+//     return a * b;
+//   }
+//   divide(a, b) {
+//     return a / b;
+//   }
 // }
 
 module.exports = class {
-    add(a, b){
-        return a + b;
-    }
-    multiply(a, b){
-        return a * b;
-    }
-    divide(a, b){
-        return a / b;
-    }
-}
+  add(a, b) {
+    return a + b;
+  }
+  multiply(a, b) {
+    return a * b;
+  }
+  divide(a, b) {
+    return a / b;
+  }
+};
